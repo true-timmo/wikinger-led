59f44c8a3f904fd25bbaec0d3793bff8c2ec67da
Timmo Schulte <timmoschulte@gmail.com>
2024-09-16 14:03:45 +0200

    Simplify this timed darkness handler a lot

--- include/LimitedDarknessHandler.h
+++ include/LimitedDarknessHandler.h
@@ -10,7 +10,6 @@ class LimitedDarknessHandler: public DarknessHandler
 {
 private:
     unsigned long lightTimelimit = 0;
-    bool lightsTimedOut = false;
     
 public:
     LimitedDarknessHandler(WebSocketEventHandler* eventHandler, unsigned long lightTimelimit = 0): DarknessHandler(eventHandler) {
@@ -19,21 +18,10 @@ public:
     
     void handleDarkness(bool isDark)
     {
-        if (isDark && this->lightsTimedOut) {
-            return;
-        }
-
         if (isDark && millis() > (this->sunsetTimestamp + this->lightTimelimit)) {
-            this->lightsTimedOut = true;
-            this->switchOff();
-
-            return;
+            isDark = false;
         }
         
-        if (!isDark && this->lightsTimedOut) {
-            this->lightsTimedOut = false;
-        }
-
         DarknessHandler::handleDarkness(isDark);
     };
 };
